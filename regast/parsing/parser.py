85736774e1179fa8052f381d19d348a35326b6bd
MiloTruck <milotruckctf@gmail.com>
2022-12-11 22:56:29 +0800

    Import solidity_antlr using source files

--- regast/parsing/parser.py
+++ regast/parsing/parser.py
@@ -1,11 +1,21 @@
+import os
 from antlr4 import FileStream, CommonTokenStream
-from importlib import import_module
+from importlib.machinery import SourceFileLoader
+
+from regast.utilities.definitions import ROOT_DIR
+
+solidityLexer = None
+solidityParser = None
 
 class Parser:
     def __init__(self):
-        self.parser_module = import_module('regast.parsing.solidity_antlr4.SolidityParser')
-        self.lexer_module = import_module('regast.parsing.solidity_antlr4.SolidityLexer')
+        solidity_antlr4_path = os.path.join(ROOT_DIR, 'build', 'solidity_antlr4')
+
+        global solidityParser, solidityLexer
+        solidityLexer = SourceFileLoader('SolidityLexer', os.path.join(solidity_antlr4_path, 'SolidityLexer.py')).load_module()
+        solidityParser = SourceFileLoader('SolidityParser', os.path.join(solidity_antlr4_path, 'SolidityParser.py')).load_module()
 
     def parse_file(self, fname: str):
-        lexer = self.lexer_module.SolidityLexer(FileStream(fname))
-        parser = self.parser_module.SolidityParser(CommonTokenStream(lexer))
+        lexer = solidityLexer.SolidityLexer(FileStream(fname))
+        parser = solidityParser.SolidityParser(CommonTokenStream(lexer))
+        print(parser.sourceUnit().contractDefinition()[0].identifier().getText())
