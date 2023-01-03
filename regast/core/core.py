3627c446fa90cb131a5b0a5c24947d801c6b823b
MiloTruck <milotruckctf@gmail.com>
2023-01-03 19:33:05 +0800

    Fixed get_instances_of

--- regast/core/core.py
+++ regast/core/core.py
@@ -13,18 +13,37 @@ class Core:
     def children(self) -> List:
         return []
 
-    def get_instances_of(self, core_type: Union[str, type]):
+    def get_instances_of(self, *core_types: List[Union[str, type]]) -> Union[List, List[List]]:
         """
-        Returns all children instances of a certain core type
+        Returns all child instances of core types specified
+        - Each core type can either be a class or str (LocalVariable, "LocalVariable")
+        - If only 1 core type is provided, returns a single list
+        - If multiple core types are provided, returns a list of lists in the same order
 
-        TODO Can this be optimized? Let's say I want to find instances of multiple core_types at the same time,
-        would there be a way to only iterate once?
+        Examples:
+        local_variables = a.get_instances_of(LocalVariable)
+        local_variables, state_variables = a.get_instances_of(LocalVariable, "StateVariable")
         """
-        # Recursively get all instances of core_type
-        instances = self.children + [x.get_instances_of(core_type) for x in self.children]
+        instances = [[] for _ in range(len(core_types))]
+        
+        for child in self.children:
+            # Check if child matches any of the core types
+            for i in range(len(core_types)):
+                core_type = core_types[i]
+                if child.__class__.__name__ == str(core_type) or isinstance(child, core_type):
+                    instances[i].append(child)
+            
+            # Recursively do this for children too, and add the results to our current ones
+            child_instances = child.get_instances_of(*core_types)
+            if len(core_types) == 1:            
+                child_instances = [child_instances]
+            instances = [instances[x] + child_instances[x] for x in range(len(core_types))]
+        
+        # Remove duplicates
+        instances = [list(set(x)) for x in instances]         
 
-        # Ensure there are no duplicates (eg. VariableDeclarationStatement and LocalVariable)
-        return list(set(instances))        
+        # Return a single list if only one core type is specified
+        return instances[0] if len(core_types) == 1 else instances
 
     def __hash__(self):
         return hash(self.tree_sitter_node)
